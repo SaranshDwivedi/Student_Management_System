6833772f06cbb954b3e7632212535966c5bc7c2e
admin <admin@DESKTOP-OUT6V2T>
2024-01-24 18:11:57 +0530

    added new class

---
+++ src/test/java/mavenPractice/mavenTest.java
@@ -0,0 +1,11 @@
+package mavenPractice;
+
+import org.testng.reporters.jq.Main;
+
+public class mavenTest {
+	public static void main(String[] args) throws InterruptedException 
+	{
+		
+	System.out.println("maven test");
+	}
+}
